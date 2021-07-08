398f3bea6ba7b4325980ded3b784faf4adeabd68
Veniamin Shitikov <sven1980@list.ru>
2021-07-08 18:17:28 +0300

    feat: added components examples

---
+++ 03-components/nuxtjs-demos/project-1/pages/test.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="test">
+    <div class="test__column test__text">
+      <h2>Text</h2>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore commodi error eaque recusandae qui voluptas sed cupiditate aperiam quidem libero reprehenderit consequatur exercitationem nulla repudiandae odio ad, ab perferendis illo.</p>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore commodi error eaque recusandae qui voluptas sed cupiditate aperiam quidem libero reprehenderit consequatur exercitationem nulla repudiandae odio ad, ab perferendis illo.</p>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore commodi error eaque recusandae qui voluptas sed cupiditate aperiam quidem libero reprehenderit consequatur exercitationem nulla repudiandae odio ad, ab perferendis illo.</p>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore commodi error eaque recusandae qui voluptas sed cupiditate aperiam quidem libero reprehenderit consequatur exercitationem nulla repudiandae odio ad, ab perferendis illo.</p>
+    </div>
+    <div class="test__column test__faq">
+      <h2>FAQ</h2>
+      
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
+
+<style scoped>
+  .test {
+    display: flex;
+    justify-content: space-between;
+    width: 80%;
+    margin: 40px auto;
+  }
+
+  .test__column {
+    width: calc(50% - 20px);
+  }
+</style>
---
+++ 03-components/nuxtjs-demos/project-2/pages/index.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="index">
+    <button class="index__button" @click="showPopup">Показать аккордионы</button>
+    <Overlay :shown="overlayShown" @overlayClick="hidePopup"/>
+    <PopUp :opened="popupOpened">
+      Popup content
+    </PopUp>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      overlayShown: false,
+      popupOpened: false
+    }
+  },
+  methods: {
+    showPopup() {
+      this.overlayShown = true;
+      this.popupOpened = true;
+    },
+    hidePopup() {
+      this.overlayShown = false;
+      this.popupOpened = false;
+    }
+  }
+}
+</script>
+
+<style>
+  .index {
+    width: 80%;
+    margin: 40px auto;
+    min-height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .index__button {
+    border: 1px solid black;
+    padding: 40px;
+    cursor: pointer;
+    background-color: transparent;
+    font-size: 24px;
+  }
+
+  .index__button:hover {
+    background-color: black;
+    color: white;
+  }
+</style>
