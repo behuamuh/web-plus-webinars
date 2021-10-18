9c9be10fb32294be8a229355e93b32a89ea33274
Veniamin Shitikov <sven1980@list.ru>
2021-10-18 20:24:32 +0300

    feat: 15-validation done

--- 15-validation/scripts/index.js
+++ 15-validation/scripts/index.js
@@ -1,13 +1,5 @@
+import { enableValidation } from './validate.js';
+
 console.log('start work');
 
-const enableValidation = () => {
-  // find all forms
-  // set event lesteners each form
-  // prevent page reload on form submit
-  // find all inputs
-  // find submit button
-  // add listeners for each input
-  //// check each input is valid // if valid, hide error else show error
-  //// toggle button state // if form valid enable button else disable
-  // set initial button state
-};
+enableValidation();
