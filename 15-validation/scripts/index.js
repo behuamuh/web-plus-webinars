5a6761371b2186d66bccb376220f4b2ab908ff6c
Veniamin Shitikov <sven1980@list.ru>
2021-10-18 17:43:55 +0300

    feat: added html & styles

---
+++ 15-validation/scripts/index.js
@@ -0,0 +1,13 @@
+console.log('start work');
+
+const enableValidation = () => {
+  // find all forms
+  // set event lesteners each form
+  // prevent page reload on form submit
+  // find all inputs
+  // find submit button
+  // add listeners for each input
+  //// check each input is valid // if valid, hide error else show error
+  //// toggle button state // if form valid enable button else disable
+  // set initial button state
+};
