9fce61d705875cb264fbae0d1679280e9558bdec
Veniamin Shitikov <sven1980@list.ru>
2021-12-21 04:09:43 +0300

    feat: done 21

--- 21-vue-vs-react/vue-app/src/App.vue
+++ 21-vue-vs-react/vue-app/src/App.vue
@@ -1,19 +1,21 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <RedBox> Hello in box! </RedBox>
+    <HelloWorld msg="Welcome to Your Vue.js App!!!!" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import RedBox from "./components/RedBox.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+    RedBox,
+  },
+};
 </script>
 
 <style>
---
+++ 21-vue-vs-react/vue-app/src/components/RedBox.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "RedBox",
+};
+</script>
+
+<style scoped>
+div {
+  background-color: red;
+}
+</style>
